8dceb27e8c5674d4ace6ec55e52291e3661f96e6
codefrofun <blackpinkaricat@gmail.com>
2025-01-06 09:53:08 -0400

    Initial commit

---
+++ GameProgramming2_06012025_WeaponClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GameProgramming2_06012025_WeaponClass
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Weapon myWeapon = new Weapon();
+            myWeapon.WeaponAttack();
+        }
+
+    }
+
+
+    public class Weapon
+    {
+        private int durablility = 50;
+        private int damageSword = 15;//change numbers after testing
+        private int enemyHealth = 50;
+
+        public void enemyHealthUpdate()
+        {
+            enemyHealth -= damageSword;
+            return;
+        }
+
+
+        public void WeaponAttack()
+        {
+
+            Console.WriteLine("Weapon durablility is 50. \n Enemy health is 50.");
+
+            Console.WriteLine("Attacking enemy...");
+
+            enemyHealthUpdate();
+
+            Console.WriteLine($"Damage is" + damageSword, "enemy health is now" + enemyHealthUpdate);
+
+            Console.WriteLine("Weapon durablility is now 40");
+
+            // attack enemy with damage
+            // on screen, display enemies health minus the damage it has taken
+        }
+    }
+}
---
+++ GameProgramming2_06012025_WeaponClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GameProgramming2_06012025_WeaponClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("GameProgramming2_06012025_WeaponClass")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f26c0775-69b8-4685-a0f5-37c3e1e825d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
